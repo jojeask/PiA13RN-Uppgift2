1451065f50bd12f21133d0c3bf9878e1878cacdc
Johan Asklund <jojeask@gmail.com>
2024-09-23 15:22:29 +0200

    Räknare + 3 knappar

---
+++ app/index.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Button, Text, View } from "react-native";
+
+export default function Index() {
+
+  const [counter, setCounter] = useState(0);
+
+
+  function addition() {
+    setCounter(counter + 1);
+  }
+
+  function subtraction() {
+    setCounter(counter - 1);
+    if (counter <= 0) {
+      setCounter(0);
+    }
+  }
+
+  function reset() {
+    if (counter > 0) {
+      setCounter(0);
+    }
+  }
+
+
+  return (
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+      }}
+    >
+      <Text style={{ fontSize: 80 }}>{counter}</Text>
+
+      <Button
+        onPress={addition}
+        title="Plus"
+      />
+
+      <Button
+        onPress={subtraction}
+        title="Minus"
+      />
+
+      <View style={{ flex: 0.95 }}></View>
+
+      {counter > 0 &&
+        <Button
+          onPress={reset}
+          title="Reset"
+        />
+      }
+
+    </View>
+  );
+}
